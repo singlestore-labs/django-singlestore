0a282936bd934b761094f7a67de5b37dcd7bba66
Pavlo Mishchenko <pmishchenko-ua@singlestore.com>
2025-04-16 21:35:14 +0300

    Override some JSON and TEXT functions

--- django_singlestore/functions.py
+++ django_singlestore/functions.py
@@ -1,6 +1,7 @@
-from django.db.models.functions.comparison import JSONObject, TextField
-from django.db.models.functions import Random, Cast
-# from django.db.models.fields.json import HasKeyLookup
+from django.db.models.fields import TextField
+from django.db.models.fields.json import HasKeyLookup, KeyTransform
+
+from django.db.models.functions import Random, Cast, JSONObject, Repeat, RPad, Length
 
 
 def random(self, compiler, connection, **extra_context):
@@ -32,14 +33,33 @@ def json_object(self, compiler, connection, **extra_context):
     )
 
 
-# def json_key_lookup(self, compiler, connection):
-#     return self.as_sql(
-#         compiler, connection, template="JSON_MATCH_ANY(%s.%%s)"
-#     )
+def json_extract(self, compiler, connection):
+    lhs, params, key_transforms = self.preprocess_lhs(compiler, connection)
+
+    all_params = []
+    for key in key_transforms:
+        lhs = f"JSON_EXTRACT_JSON({lhs}, %s)"
+        all_params.append(key)
+
+    return lhs, list(params) + all_params
+
+
+def json_key_lookup(self, compiler, connection):
+    return self.as_sql(
+        compiler, connection, template="JSON_MATCH_ANY_EXISTS(%s, %%s)"
+    )
+    
+def repeat(self, compiler, connection, **extra_context):
+    expression, number = self.source_expressions
+    length = None if number is None else Length(expression) * number
+    rpad = RPad(expression, length, expression)
+    return rpad.as_sql(compiler, connection, **extra_context)
 
 
 def register_functions():
     Random.as_singlestore = random
     Cast.as_singlestore = cast
     JSONObject.as_singlestore = json_object
-    # HasKeyLookup.as_singlestore = json_key_lookup
+    HasKeyLookup.as_singlestore = json_key_lookup
+    KeyTransform.as_singlestore = json_extract
+    Repeat.as_singlestore = repeat
