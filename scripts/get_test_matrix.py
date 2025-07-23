79a5b72fcb73ce036b7fe31a1c56a8bb917ffbfe
Parth Sharma <psharma@singlestore.com>
2025-07-23 16:46:16 +0530

    Fix of get_object_or_404 tests

--- scripts/get_test_matrix.py
+++ scripts/get_test_matrix.py
@@ -6,7 +6,7 @@ DEFAULT_NUM_GROUPS_FOR_TESTS = 5
 
 
 def get_test_modules(tests_root):
-    subdirs_to_skip = {"import_error_package", "test_runner_apps", "__pycache__", "requirements"}
+    subdirs_to_skip = {"import_error_package", "test_runner_apps", "__pycache__", "requirements", "gis_tests"}
     test_modules = []
     for item in os.listdir(tests_root):
         full_path = os.path.join(tests_root, item)
